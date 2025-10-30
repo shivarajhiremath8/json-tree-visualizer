a0b8babcf42a7c117ccc8070ef3202bf061a3a56
Shivaraj Hiremath <shivaraj94826@gmail.com>
2025-10-30 10:45:49 +0530

    initial setup

--- Assignment/src/App.jsx
+++ Assignment/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
     <>
       <div className="bg-gray-800 min-h-screen flex flex-col items-center justify-center text-white">
         <p className="text-2xl mb-4">
-          Click on the Vite and React logos to learn more
+          React
         </p>
       </div>
 
